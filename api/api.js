ac6636078b014e0a3a83fa5d851ce6ce11a836b2
Afshana Zaman <88719256+afshana22@users.noreply.github.com>
2022-02-13 23:23:46 +0530

    Create api.js

---
+++ api/api.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+export const api = axios.create({
+  baseURL:
+   
+
+    "https://www.themealdb.com/api/json/v1",
+
+  headers: { "Content-Type": "application/json" },
+});
