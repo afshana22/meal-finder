27344e92c2baa04e05600eba2c42a2bc0661ff5f
Afshana Zaman <88719256+afshana22@users.noreply.github.com>
2022-02-13 23:21:40 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,15 @@
 
+import "./App.css";
+
+import { Provider } from "react-redux";
+import store from "./store/Store";
+import SearchMeal from "./SearchMeal";
+function App() {
+  return (
+    <Provider store={store}>
+      <SearchMeal />
+    </Provider>
+  );
+}
+
+export default App;
